5799840a4b4fc719f1580fe9a422420ffe801ab9
Tannkely <71130779+Tannkely@users.noreply.github.com>
2020-11-08 00:55:40 +0300

    Правки всех страниц. Подключение JS.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,52 @@
-const formLink=document.querySelector(".button-contacts"),formPopup=document.querySelector(".modal-window"),formClose=formPopup.querySelector(".modal-window-close"),formName=formPopup.querySelector(".name-user"),formForm=formPopup.querySelector(".modal-window-form"),formEmail=formPopup.querySelector(".email-user"); try{storage=localStorage.getItem("form")}catch(e){isStorageSupport=!1}formLink.addEventListener("click",function(e){e.preventDefault(),formPopup.classList.add("modal-window-show"),storage?(formName.value=storage,formEmail.focus()):formName.focus()}),formClose.addEventListener("click",function(e){e.preventDefault(),formPopup.classList.remove("modal-window-show"),formPopup.classList.remove("modal-window-error")}),formForm.addEventListener("submit",function(e){formName.value&&formEmail.value?localStorage.setItem("form",formName.value):(e.preventDefault(),formPopup.classList.add("modal-window-error"),formPopup.offsetWidth=formPopup.offsetWidth,formPopup.classList.add("modal-window-error"))}),window.addEventListener("keydown",function(e){27===e.keyCode&&formPopup.classList.contains("modal-window-show")&&(e.preventDefault(),formPopup.classList.remove("modal-window-show"),formPopup.classList.remove("modalwindow-error"))});
+const buttonOpen = document.querySelector('#btn-contact-modal');
+const buttonClose = document.querySelector('#btn-close-modal');
+const modal = document.querySelector('#contact-modal');
 
-const formLink = document.querySelector('.button-contacts'); 
-const formPopup = document.querySelector('.modal');
-const formClose = formPopup.querySelector('.modal-close');
-const formName = formPopup.querySelector('.name-user');
-const formForm = formPopup.querySelector('.modal-form');
-const formEmail = formPopup.querySelector('.email-user');
+function openModal(e) {
+    e.preventDefault();
+    modal.classList.add('modal-window-show');
+}
+
+function closeModal() {
+    modal.classList.remove('modal-window-show');
+}
+
+buttonClose.addEventListener('click', closeModal);
+buttonOpen.addEventListener('click', openModal);
+
+
+const slides = document.querySelector('.slides');
+
+if (slides) {
+    const slideOne = slider.querySelector('.slide-control-one');
+  const slideTwo = slider.querySelector('.slide-control-two');
+  const slideThree = slider.querySelector('.slide-control-three');
+  const controlOne = slider.querySelector('.slider-control-one');
+  const controlTwo = slider.querySelector('.slider-control-two');
+  const controlThree = slider.querySelector('.slider-control-three');
+  controlOne.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    controlOne.classList.add('slider-control-active');
+    slideOne.classList.add('slide-current');
+
+    controlTwo.classList.remove('slider-control-active');
+    slideTwo.classList.remove('slide-current');
+    controlThree.classList.remove('slider-control-active');
+    slideThree.classList.remove('slide-current');
+  });
+
+}
+
+controlThree.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    controlThree.classList.add('slider-control-active');
+    slideThree.classList.add('slide-current');
+
+    controlOne.classList.remove('slider-control-active');
+    slideOne.classList.remove('slide-current');
+    controlTwo.classList.remove('slider-control-active');
+    slideTwo.classList.remove('slide-current');
+  });
+}
